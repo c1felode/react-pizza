3bd613205fc51df25b9c7f93dc6a1c86793accbe
c1felode <abdusamadov.workit@gmail.com>
2026-07-16 23:42:11 +0500

    redux logic cleared

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,14 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import filter from "./slices/filterSlice.ts";
-import cart from './slices/cartSlice.ts'
-import pizza from './slices/pizzaSlice.ts'
 import { useDispatch } from 'react-redux';
 
 export const store = configureStore({
     reducer: {
-        filter,
-        cart,
-        pizza
     },
 })
 
